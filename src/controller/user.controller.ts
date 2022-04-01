ed424a570e4f41cdca54f813e6c2e763bc304ff9
nicolas <nicogrunfeld@gmail.com>
2022-04-01 17:48:50 -0300

    configure ioc with inversify

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,9 @@
 import { UserEntity } from '../entities/user.entity';
 import { Route, Controller, Get, Tags } from 'tsoa';
+import { provideSingleton } from '../utils/provideSingleton';
+import 'reflect-metadata';
 
+@provideSingleton(UserController)
 @Tags('users')
 @Route('users')
 export class UserController extends Controller {
---
+++ src/ioc.ts
@@ -0,0 +1,15 @@
+import { Container, decorate, injectable } from 'inversify';
+import { buildProviderModule } from 'inversify-binding-decorators';
+import { Controller } from 'tsoa';
+import 'reflect-metadata';
+
+// Create a new container tsoa can use
+const iocContainer = new Container();
+
+decorate(injectable(), Controller); // Makes tsoa's Controller injectable
+
+// make inversify aware of inversify-binding-decorators
+iocContainer.load(buildProviderModule());
+
+// export according to convention
+export { iocContainer };
