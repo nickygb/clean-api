8b608748a0075ffb591e7389365c2f014248e2c9
nicolas <nicogrunfeld@gmail.com>
2022-04-02 20:09:48 -0300

    docke-compose mysql + knex.connection provider

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,19 @@
+import dotenv from 'dotenv';
+import { resolve as pathResolve } from 'path';
+
+const { env } = process;
+dotenv.config({ path: pathResolve(__dirname, `./env/.env.${env.NODE_ENV}`) });
+
 export const config = {
   port: 5000,
+  AWS: {
+    region: env.AWS_REGION,
+  },
+  AURORA: {
+    db: env.AURORA_DB,
+    secretArn: env.AURORA_SECRET_ARN,
+    resourceArn: env.AURORA_RESOURCE_ARN,
+    port: env.AURORA_PORT,
+    endpoint: env.AURORA_ENDPOINT,
+  },
 };
